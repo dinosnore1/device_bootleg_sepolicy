LineageOS/android_device_lineage_sepolicy

sepolicy: Break livedisplay hal policy into impl independent ones
 * Livedisplay has different backends (QDCM, sysfs, etc.), QDCM impl
   doesn't use sysfs and sysfs impl doesn't use QC display service,
   don't abuse the "default" policy.
 * Add policy for system variant qti hal while at it.

Change-Id: I87725a091ebe5db5beeb1619ce4daaac9636d808

qcom/sepolicy.mk
@@ -6,7 +6,12 @@
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lineage/sepolicy/qcom/private
 
-ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/qcom/dynamic \
+    device/lineage/sepolicy/qcom/priv_vendor
+else
 BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/qcom/dynamic \
     device/lineage/sepolicy/qcom/vendor
 endif

dinosnore1/device_bootleg_sepolicy

 * LiveDisplay has different backends (QDCM, sysfs, etc.). QDCM impl
   doesn't use sysfs, sysfs impl doesn't use QC display service and
   vice versa, so don't abuse the "default" policy.

qcom/sepolicy.mk
@@ -12,7 +12,11 @@ endif
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/bootleg/sepolicy/qcom/private
 
-ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/bootleg/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/bootleg/sepolicy/qcom/dynamic \
     device/bootleg/sepolicy/qcom/vendor
 endif
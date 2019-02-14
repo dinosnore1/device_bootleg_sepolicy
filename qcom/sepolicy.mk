71bf675bfc6f8778f5f01aaf0eadebf25bb96f3b
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 21:29:46 +0800

    sepolicy: Break livedisplay hal policy into impl independent ones
     * LiveDisplay has different backends (QDCM, sysfs, etc.). QDCM impl
       doesn't use sysfs, sysfs impl doesn't use QC display service and
       vice versa, so don't abuse the "default" policy.
    
    Change-Id: I87725a091ebe5db5beeb1619ce4daaac9636d808

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -12,7 +12,11 @@ endif
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/bootleg/sepolicy/qcom/private
 
-ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/bootleg/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/bootleg/sepolicy/qcom/dynamic \
     device/bootleg/sepolicy/qcom/vendor
 endif
